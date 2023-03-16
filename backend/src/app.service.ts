880a4356df68c7e6c48807cf2ddfd234a98ee86b
Rulong Huang <hrlzmyy@gmail.com>
2023-03-16 14:22:46 -0400

    test pipeline

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Prolouge_test!';
+    return 'Hello Prolouge_test_first_commit!';
   }
 }
